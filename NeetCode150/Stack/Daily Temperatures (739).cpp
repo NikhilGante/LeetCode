56cbb4c71b301fa0cdb0d7a687fd5e04f6dd5a20
Nikhil Gante <odyseus05@gmail.com>
2025-05-09 15:38:26 -0400

    Solved Daily Temperatures (739)

---
+++ NeetCode150/Stack/Daily Temperatures (739).cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    public:
+        vector<int> dailyTemperatures(vector<int>& temperatures) {
+            stack<int> s;
+            int n = temperatures.size();
+            vector<int> ans(n);
+    
+            for (int i = 0; i < n; i++){
+                while(!s.empty() && temperatures[i] > temperatures[s.top()]){
+                    ans[s.top()] = i - s.top();
+                    s.pop();
+                }
+                s.push(i);
+            }
+            return ans;
+        }
+};
