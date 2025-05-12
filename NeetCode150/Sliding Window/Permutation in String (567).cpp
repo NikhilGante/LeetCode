886262007d5813c3ad56b1ecad7f092458b31e2e
Nikhil Gante <odyseus05@gmail.com>
2025-05-12 00:35:31 -0400

    Solved Permutation in String (567) (more optimal but still with hashmap)

--- NeetCode150/Sliding Window/Permutation in String (567).cpp
+++ NeetCode150/Sliding Window/Permutation in String (567).cpp
@@ -6,37 +6,37 @@ class Solution {
         bool checkInclusion(string s1, string s2) {
             if(s1.size() > s2.size())   return false;
             unordered_map<char, int> substr_freq, window_freq;
-            unordered_map<char, vector<int>> window_locations;
-            int window_matching_count = 0; // How many letters of the window match the substr in frequency 
+            int matches = 0; // How many letters of the window match the substr in frequency 
 
             for(char c: s1) substr_freq[c]++;
 
-            for(int r = 0; r < s2.size(); r++){
+            int l = 0, r = 0;
+            for(; r < s1.size(); r++) window_freq[s2[r]]++;
+
+            for(const auto& p: window_freq){
+                if(substr_freq.find(p.first) != substr_freq.end() && window_freq[p.first] == substr_freq[p.first]){
+                    matches++;
+                }
+            }
+
+            for(; r < s2.size(); r++){
+                if(matches == substr_freq.size())   return true;
+
                 char c = s2[r];
                 window_freq[c]++;
-                window_locations[c].push_back(r);
+                if(substr_freq.find(c) != substr_freq.end()){
+                    if(window_freq[c] == substr_freq[c])    matches++;
+                    else if(window_freq[c] == substr_freq[c] + 1)    matches--;
+                }
 
-                bool found = substr_freq.find(c) != substr_freq.end();
-                if(!found){   // window no longer matches, have to reset
-                    window_freq.clear();
-                    window_locations.clear();
-                    window_matching_count = 0;
+                window_freq[s2[l]]--;
+                if(substr_freq.find(s2[l]) != substr_freq.end()){
+                    if(window_freq[s2[l]] == substr_freq[s2[l]])    matches++;
+                    else if(window_freq[s2[l]] == substr_freq[s2[l]] - 1)    matches--;
                 }
-                else if(window_freq[c] > substr_freq[c]){   // Window freq exceeds substr_freq for character c 
-                    int first_index = window_locations[c][0];
-                    window_freq.clear();
-                    window_locations.clear();
-                    window_matching_count = 0;
-                    for(int i = first_index + 1; i <= r; i++){
-                        window_freq[s2[i]]++;
-                        window_locations[s2[i]].push_back(i);
-                        if(found && window_freq[s2[i]] == substr_freq[s2[i]])   window_matching_count++;
-                    }
-                }
-                else if(found && window_freq[c] == substr_freq[c])   window_matching_count++; // "Normal" case: char found in substr
-                
-                if(window_matching_count == substr_freq.size())   return true;
+
+                l++;
             }
-            return false;
+            return matches == substr_freq.size();
         }
 };
