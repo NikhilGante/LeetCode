74df83baf012add0b0ccf8bcb785d0fa17bfbe6a
Nikhil Gante <odyseus05@gmail.com>
2025-05-13 17:03:52 -0400

    Fixed suboptimal sol to Sliding Window Maximum (239)

--- NeetCode150/Sliding Window/Sliding Window Maximum (239).cpp
+++ NeetCode150/Sliding Window/Sliding Window Maximum (239).cpp
@@ -6,19 +6,21 @@ class Solution {
         vector<int> maxSlidingWindow(vector<int>& nums, int k) {
             int n = nums.size();
             priority_queue<int> pq;
+            unordered_map<int, int> non_window_freq;   // (val, num_occurences)
             vector<int> ans(nums.size() - k + 1);
             
             int l = 0, r = 0;
-            for (; r < k; r++){
-                pq.push(nums[r]);
-            }
+            for (; r < k; r++)  pq.push(nums[r]);
             ans[l] = pq.top();
-            for(; r < nums.size(); r++){
-                while(nums[r] > pq.top() && pq.size() > k - 1){
-                    pq.push(nums[r]);
+            for (; r < nums.size(); r++){
+                non_window_freq[nums[l]]++;
+                while(non_window_freq[pq.top()]){
+                    non_window_freq[pq.top()]--;
+                    pq.pop();
                 }
-                ans[l] = pq.top();
+                pq.push(nums[r]);
                 l++;
+                ans[l] = pq.top();
             }
             return ans;         
         }
