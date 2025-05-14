f4e32c08264d4b3ae22474a95cc8e11199443078
Nikhil Gante <odyseus05@gmail.com>
2025-05-14 00:08:36 -0400

    Solved Sliding Window Maximum (239) Optimally with Deque

--- NeetCode150/Sliding Window/Sliding Window Maximum (239).cpp
+++ NeetCode150/Sliding Window/Sliding Window Maximum (239).cpp
@@ -3,6 +3,27 @@ using namespace std;
 
 class Solution {
     public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector<int> ans(nums.size() - k + 1);
+        deque<int> d;
+
+        int l = 0;
+        for (int r = 0; r < nums.size(); r++){
+            while(!d.empty() && nums[r] > nums[d.back()])  d.pop_back();    // Pop old values if less than new value
+
+            d.push_back(r);
+            if(!d.empty() && r - d.front() + 1 > k)    d.pop_front();   // make sure window doesn't exceed size
+
+            if(r >= k - 1){ // Fill answer array
+                ans[l] = nums[d.front()];   // Front of deque should always hold the max val in the window
+                l++;
+            }            
+        }
+        return ans;               
+    }
+
+        // Old solution (suboptimal - uses priority queue)
+        /*
         vector<int> maxSlidingWindow(vector<int>& nums, int k) {
             int n = nums.size();
             priority_queue<int> pq;
@@ -24,4 +45,5 @@ class Solution {
             }
             return ans;         
         }
+        */
 };
