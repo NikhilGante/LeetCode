1fd832a640d7785593847667b4e3510fd24f6a5b
Nikhil Gante <odyseus05@gmail.com>
2025-05-15 17:10:51 -0400

    Solved 3Sum (suboptimally with hashing)

---
+++ Misc Problems/3sum.py
@@ -0,0 +1,53 @@
+from typing import List
+from collections import defaultdict
+
+# class Solution:
+    
+
+# nums.sort()
+# n = len(nums)
+# for i in range(n):
+#     if i and nums[i] == nums[i-1]:
+#         continue
+#     comp = -nums[i]
+#     l = i + 1
+#     r = n - 1
+#     while l < r:
+#         guess = nums[l] + nums[r]
+#         if guess == comp:
+#             ans.append([nums[i], nums[l], nums[r]])
+#             l += 1
+#             while nums[l] == nums[l - 1] and l < r:
+#                 l += 1
+#         elif guess < comp:
+#             l += 1
+#         else:
+#             r -= 1
+
+# return ans
+
+# Suboptimal solution (with hashing)
+
+class Solution2:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        mp = {}
+        ans = set()
+        for i, val in enumerate(nums):
+            if val not in mp:
+                mp[val] = []
+            mp[val].append(i)
+
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] or nums[j]:          # Skip 0, 0 combinations
+                    complement = -(nums[i] + nums[j])
+                    if complement in mp:
+                        for k in mp[complement]:
+                            if k != i and k != j:
+                                ans.add(tuple(sorted([nums[i], nums[j], nums[k]])))
+                                break
+
+        if len(mp.get(0, [0])) >= 3:
+            ans.add((0, 0, 0))
+    
+        return list(ans)
