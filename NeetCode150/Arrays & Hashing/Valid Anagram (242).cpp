1f98f2a4e0862dcea7c9729daf8f8d3ed73f71ea
Nikhil Gante <odyseus05@gmail.com>
2025-06-06 23:25:48 -0400

    Solved Valid Anagram (242)

---
+++ NeetCode150/Arrays & Hashing/Valid Anagram (242).cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    public:
+        bool isAnagram(string s, string t) {
+            if(s.size() != t.size())    return false;
+            unordered_map<char, int> s_map, t_map;
+            for(int i = 0; i < s.size(); i++){
+                s_map[s[i]]++;
+                t_map[t[i]]++;
+            }
+            return t_map == s_map;
+        }
+    };
