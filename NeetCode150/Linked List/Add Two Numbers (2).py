0fe07aed44708ddaaca8011f1ac467735c68d8b8
Nikhil Gante <odyseus05@gmail.com>
2025-06-12 23:53:08 -0400

    Solved Add Two Numbers (2)

--- NeetCode150/Linked List/Add Two Numbers (2).py
+++ NeetCode150/Linked List/Add Two Numbers (2).py
@@ -5,7 +5,7 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-        
+
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         root = ListNode(0)
@@ -17,7 +17,7 @@ class Solution:
             if l1:
                 val += l1.val
                 l1 = l1.next
-            if l2:
+            if l2:  
                 val += l2.val
                 l2 = l2.next
 
