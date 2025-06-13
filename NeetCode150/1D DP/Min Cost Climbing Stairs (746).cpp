83236dfcca8878f6df538c87068afbfd29e6ad15
Nikhil Gante <odyseus05@gmail.com>
2025-06-13 17:29:46 -0400

    Solved Min Cost Climbing Stairs (746).cpp with O(n) space solution

---
+++ NeetCode150/1D DP/Min Cost Climbing Stairs (746).cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n = cost.size();
+        vector<int> ans(n + 1);
+        ans[0] = ans[1] = 0;
+        for(int i = 2; i <= n; i++){
+            ans[i] = min(cost[i - 1] + ans[i - 1], cost[i - 2] + ans[i - 2]);
+        }
+        return ans[n];
+    }
+};
