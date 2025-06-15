d91d1903fde18dae3ed5844036052e4f634098a7
Nikhil Gante <odyseus05@gmail.com>
2025-06-15 14:58:51 -0400

    Solved Coin Change (322) with bottom up approach

--- NeetCode150/1D DP/Coin Change (322).py
+++ NeetCode150/1D DP/Coin Change (322).py
@@ -1,22 +1,41 @@
 from typing import List
 
+# Tabulation (Bottom up) approach
 class Solution:
-    def dfs(self, coins: List[int], amount: int) -> int:
-        if not amount:
-            return 0
-        if amount in self.memo:
-            return self.memo[amount]
-        min_coins = float('inf')
-        for i in coins:
-            if i <= amount:
-                num_coins = self.dfs(coins, amount - i)
-                if num_coins != -1:
-                    min_coins = min(min_coins, 1 + num_coins)
-        self.memo[amount] = -1 if min_coins == float('inf') else min_coins
-        return self.memo[amount]
-
     def coinChange(self, coins: List[int], amount: int) -> int:
-        self.memo = {}
-        return  self.dfs(sorted(coins, reverse = True), amount)
+        coins.sort()
+        dp = [-1] * (amount + 1)
+        dp[0] = 0
+        for num in range(amount + 1):
+            for coin in coins:
+                if coin > num:
+                    break
+                elif coin == num:
+                    dp[num] = 1
+                elif dp[num - coin] != -1:
+                    dp[num] = dp[num - coin] + 1 if dp[num] == -1 else min(dp[num], dp[num - coin] + 1)
+        return dp[-1]
+
+
+# Top-down Memo Solution
+
+# class Solution:
+#     def dfs(self, coins: List[int], amount: int) -> int:
+#         if not amount:
+#             return 0
+#         if amount in self.memo:
+#             return self.memo[amount]
+#         min_coins = float('inf')
+#         for i in coins:
+#             if i <= amount:
+#                 num_coins = self.dfs(coins, amount - i)
+#                 if num_coins != -1:
+#                     min_coins = min(min_coins, 1 + num_coins)
+#         self.memo[amount] = -1 if min_coins == float('inf') else min_coins
+#         return self.memo[amount]
+
+#     def coinChange(self, coins: List[int], amount: int) -> int:
+#         self.memo = {}
+#         return  self.dfs(sorted(coins, reverse = True), amount)
 
     
