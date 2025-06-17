0d53a6e9a4fddff580ca88ccbcc6a1f0ec7a44bf
Nikhil Gante <odyseus05@gmail.com>
2025-06-17 00:02:39 -0400

    Optimized Space Shortest Path in Binary Matrix (1091)

--- NeetCode150/Graphs/Shortest Path in Binary Matrix (1091).py
+++ NeetCode150/Graphs/Shortest Path in Binary Matrix (1091).py
@@ -3,30 +3,24 @@ from collections import deque
 
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-        if grid[0][0]:
+        n = len(grid)
+        if grid[0][0] or grid[n-1][n-1]:
             return -1
 
-        n = len(grid)
-        visited = [[False] * n for _ in range(n)]
-        visited[0][0] = True
-
-        q = deque()
-        q.append((0, 0))
+        q = deque([(0, 0)])
+        grid[0][0] = 1  # store the distance IN the grid, saving memory
 
         directions = ((0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1))
-        dist = 1
         while q:
-            for _ in range(len(q)):
-                cur_r, cur_c = q.popleft()
-
-                if (cur_r, cur_c) == (n - 1, n - 1):
-                    return dist
-                for dr, dc in directions:
-                    r, c = cur_r + dr, cur_c + dc
-                    if min(r, c) < 0 or max(r, c) >= n or grid[r][c] == 1 or visited[r][c]:
-                        continue
-                    visited[r][c] = True
-                    q.append((r, c))
-            dist += 1
+            cur_r, cur_c = q.popleft()
+            dist = grid[cur_r][cur_c]
+            if (cur_r, cur_c) == (n - 1, n - 1):
+                return dist
+            for dr, dc in directions:
+                r, c = cur_r + dr, cur_c + dc
+                if min(r, c) < 0 or max(r, c) >= n or grid[r][c]:
+                    continue
+                grid[r][c] = dist + 1
+                q.append((r, c))
 
         return -1
