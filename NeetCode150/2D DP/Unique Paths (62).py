42279af9ca791d3ac081a4f1de1fd945aaf9af9d
Nikhil Gante <odyseus05@gmail.com>
2025-07-02 15:11:51 -0400

    Improved solution to Unique Paths (62)

--- NeetCode150/2D DP/Unique Paths (62).py
+++ NeetCode150/2D DP/Unique Paths (62).py
@@ -1,4 +1,14 @@
+# Space optimized solution
 class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [0] * n # row below
+        for r in range(m - 1, -1, -1):
+            dp[-1] = 1
+            for c in range(n - 2, -1, -1):
+                dp[c] += dp[c + 1]
+        return dp[0]
+    
+class Solution2:
     def uniquePaths(self, m: int, n: int) -> int:
         prev = [0] * n # row below
         for r in range(m - 1, -1, -1):
