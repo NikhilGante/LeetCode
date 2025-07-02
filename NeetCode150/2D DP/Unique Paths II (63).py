660652fc914b3e39923598cacc0b0c903bd2a9a1
Nikhil Gante <odyseus05@gmail.com>
2025-07-02 15:26:05 -0400

    Space optimized solution to Unique Paths II (63)

--- NeetCode150/2D DP/Unique Paths II (63).py
+++ NeetCode150/2D DP/Unique Paths II (63).py
@@ -1,5 +1,19 @@
 from typing import List
-class Solution:
+class Solution: # Space-optimized - O(n) space, O(n * m) space
+    def uniquePathsWithObstacles(self, grid: List[List[int]]) -> int:
+        m, n = len(grid[0]), len(grid)
+        dp = [0] * (m + 1)
+        dp[m-1] = 1
+        for r in range(n - 1, -1, -1):
+            for c in range(m - 1, -1, -1):
+                if grid[r][c]:
+                    dp[c] = 0
+                else:
+                    dp[c] += dp[c + 1]
+        return dp[0]
+
+
+class Solution2:    # Old solution
     def uniquePathsWithObstacles(self, grid: List[List[int]]) -> int:
         m, n = len(grid[0]), len(grid)
         prev = [0] * m
