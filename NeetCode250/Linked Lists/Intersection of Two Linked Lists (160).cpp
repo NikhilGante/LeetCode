a6cb701e40ced90df85331531e9c0e0b76f4abdd
Nikhil Gante <odyseus05@gmail.com>
2025-07-08 01:03:10 -0400

    Solved Intersection of Two Linked Lists (160) in O(n) time but now O(1) space

--- NeetCode250/Linked Lists/Intersection of Two Linked Lists (160).cpp
+++ NeetCode250/Linked Lists/Intersection of Two Linked Lists (160).cpp
@@ -1,12 +1,28 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
+#include <bits/stdc++.h>
+using namespace std;
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+// O(1) Space solution - basically the same as Lowest Common Ancestor of Binary Tree III
 class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode* a = headA, *b = headB;
+        while(a != b){
+            a = a? a->next : headB;
+            b = b? b->next : headA;
+        }
+        return a;
+    }
+};
+
+// O(n) space solution
+class Solution2 {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
         unordered_set<ListNode*> vis;
