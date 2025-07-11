a2aec21254bde531833fcef19cb556f423887a4e
Nikhil Gante <odyseus05@gmail.com>
2025-07-10 21:36:30 -0400

    Solved Island Perimeter (463) Efficiently with simple iteration

--- NeetCode250/Graphs/Island Perimeter (463).py
+++ NeetCode250/Graphs/Island Perimeter (463).py
@@ -1,5 +1,24 @@
 from typing import List
 
+# Simple iteration
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        m, n = len(grid[0]), len(grid)
+        directions = ((0, 1), (0, -1), (1, 0), (-1, 0))
+
+        ans = 0
+        for r in range(n):
+            for c in range(m):
+                if grid[r][c]:
+                    ans += 4 # Num of perimeter lines, assume 4 to start off
+                    for dr, dc in directions:
+                        nr, nc = r + dr, c + dc
+                        if 0 <= nr < n and 0 <= nc < m and grid[nr][nc]:
+                            ans -= 1 # Each neighbour means one less perimeter line
+
+        return ans
+
+
 # DFS Solution (Suboptimal)
 class Solution2:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
