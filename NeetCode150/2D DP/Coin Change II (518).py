0eb651592e1d8dfbf37011da5649f80f3441607d
Nikhil Gante <odyseus05@gmail.com>
2025-11-10 12:41:08 -0500

    Solved Coin Change II (518)

---
+++ NeetCode150/2D DP/Coin Change II (518).py
@@ -0,0 +1,23 @@
+from typing import List
+import heapq
+
+# O(n * m) time, O(m * n space), where n is amount, m is len(coins)
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        # dp[r][c] -> (r -> are able to use coins[0 to r], c -> generate value c)
+        dp = [[0] * (amount + 1) for _ in coins]
+        for c in range(amount + 1):  # init first row
+            if c % coins[0] == 0:
+                dp[0][c] = 1
+
+        for r in range(len(coins)):  # Init first column
+            dp[r][0] = 1        # 1 way to generate value of 0, use none of the coins
+
+        for r in range(1, len(coins)):
+            curCoin = coins[r]
+            for c in range(1, amount + 1):
+                skip = dp[r - 1][c]
+                include = dp[r][c - curCoin] if c >= curCoin else 0
+                dp[r][c] = skip + include
+
+        return  dp[-1][-1]
