c680a3490384ce4a3e1e3ff0c6f53b0daf3b47e4
Nikhil Gante <odyseus05@gmail.com>
2025-11-18 23:59:23 -0500

    Solved Number of 1 bits (191)

---
+++ NeetCode150/Bit Manipulation/Number of 1 bits (191).cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int hammingWeight(int n) {
+        int ans = 0;
+        while(n){
+            ans += n & 1;
+            n >>= 1;
+        }
+        return ans;
+    }
+};
